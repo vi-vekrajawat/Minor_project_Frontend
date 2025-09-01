47388d58a91a2545571ef6b479ee6fc011bb8eff
Vivek Singh <iamvivekrajawat@gmail.com>
2025-09-01 09:31:13 +0530

    Major Frontend Project

--- src/apis/Backend.js
+++ src/apis/Backend.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://assignmentmanagementproject.onrender.com"
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://assignmentmanagementproject.onrender.com"
+// export const BASE_URL = "http://localhost:3000"
 export default {
     STUDENT_LIST: BASE_URL+'/admin',
     USER_LOGIN: BASE_URL+'/admin/login',
